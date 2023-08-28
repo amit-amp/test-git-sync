fc90b47a95e81c6ffc086330feec52a1451815ff
Amit Barletz <amit@amplication.com>
2023-08-28 14:50:20 +0300

    test-git-sync

--- apps/test-git-sync/src/customer/customer.service.ts
+++ apps/test-git-sync/src/customer/customer.service.ts
@@ -7,4 +7,18 @@ export class CustomerService extends CustomerServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
   }
+
+  async greetingCustomer(customerId: string): Promise<string> {
+    const customer = await this.prisma.customer.findUnique({
+      where: {
+        id: customerId,
+      },
+    });
+
+    if (!customer) {
+      throw new Error("Customer not found");
+    }
+
+    return `Hello ${customer.firstName} ${customer.lastName}`;
+  }
 }
